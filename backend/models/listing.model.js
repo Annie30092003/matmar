d410f7c7aef61ba0b044d24bafee47afd954425a
Annie30092003 <97344515+Annie30092003@users.noreply.github.com>
2024-07-29 00:38:45 +0530

    Add files via upload

---
+++ backend/models/listing.model.js
@@ -0,0 +1,59 @@
+import mongoose from "mongoose";
+
+const listingSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    regularPrice: {
+      type: Number,
+      required: true,
+    },
+    discountPrice: {
+      type: Number,
+      required: true,
+    },
+    quantity: {
+      type: Number,
+      required: true,
+    },
+    fresh: {
+      type: Boolean,
+      required: true,
+    },
+    offer: {
+      type: Boolean,
+      required: true,
+    },
+    fish: {
+      type: Boolean,
+      required: true,
+    },
+    others: {
+      type: Boolean,
+      required: true,
+    },
+    imageUrls: {
+      type: Array,
+      required: true,
+    },
+    userRef: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Listing = mongoose.model("Listing", listingSchema);
+
+export default Listing;
